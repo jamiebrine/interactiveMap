9aa1d86efe9dea5972161b0de1d1a22bd9cde7d5
Jamie Brine <jamiebrine@yahoo.co.uk>
2024-07-27 14:13:16 +0100

    Implemented basic form functionality returning all matching dates. TODO - calculate wait times, make pretty

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask, request, render_template
 import socket
+import json
 
 app = Flask(__name__)
 
@@ -12,6 +13,30 @@ def index():
     except:
         return render_template('error.html')
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    band = request.form.get('band')
+    designation = request.form.get('designation')
+    postcode = request.form.get('postcode')
+    matches = calculateAverageWait(band,designation,postcode)
+
+    #return str(len(matches))
+
+    output = ""
+    for item in matches:
+        output += f"{item[0]}, {item[1]}\n"
+    return output
+
+def calculateAverageWait(band, designation, postcode):
+
+    # Load data from data.json
+    with open('src/data.json') as file:
+        data = json.load(file)
+
+    # Filter data based on band, designation, and area
+    filteredData = [(item['letDate'], item['priorityDate']) for item in data if item['band'] == band and item['designation'] == designation and item['postcode'] == postcode]
+
+    return filteredData
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
